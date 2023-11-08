1e459c29d38cee585e0de325eeb21b37952ef963
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2023-11-08 16:42:15 -0300

    updating

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const meals = document.getElementById('meals');
+
+getRandomMeal();
+
+async function getRandomMeal() {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+
+    const respData = await response.json();
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
+};
+
+async function getMealById(id) {
+    const meal = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=52772' + id);
+};
+
+async function getMealsBySearch(term) {
+    const meals = await fetch('www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata' + term);
+};
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+        <div class="meal-header">
+        ${random ? `<span class="random">Random Recipe</span>` : ''}
+            <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn"><i class="fas fa-heart"></i></button>
+            </div>
+        </div>
+    `;
+
+    meals.appendChild(meal);
+}
